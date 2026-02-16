8c83dc71fd09fc806b656e2a7623a73d92b4cc16
John Bohan <quepasajohn@hotmail.com>
2026-02-16 13:30:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -217,6 +217,5 @@ def generate():
 
 
 if __name__ == "__main__":
-    # This tells Flask to use the port Render provides
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+
